55592db050a37b7898dc919fd2cb428f8de5f946
gnarlyeah <118649910+gnarlyeah@users.noreply.github.com>
2023-03-25 07:29:25 +0800

    Update login.js

--- loginjs/login.js
+++ loginjs/login.js
@@ -14,3 +14,17 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
